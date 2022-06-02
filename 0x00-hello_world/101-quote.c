5f4ca97bd0bafb92c5c33d69980282448071921e
JamesOdera <nyangoma.odera@gmail.com>
2022-06-02 08:19:41 -0700

    add file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 81);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 80);
 	return (1);
 }
