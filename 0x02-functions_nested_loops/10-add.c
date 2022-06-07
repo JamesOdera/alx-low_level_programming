f80198582de5e7eb294fedfd5a39839f99cdb0ba
JamesOdera <nyangoma.odera@gmail.com>
2022-06-07 05:07:58 -0700

    add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a fxn that adds two intergers and runs the result
+ * @one: First number input
+ * @two: second number
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
