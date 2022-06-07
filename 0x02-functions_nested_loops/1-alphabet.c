46037adf366188a753ebd6bdd459f021042ac639
JamesOdera <nyangoma.odera@gmail.com>
2022-06-07 02:45:55 -0700

    add file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
