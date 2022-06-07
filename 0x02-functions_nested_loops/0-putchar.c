734fa7121fa9407fcb42e6d4386445a4c1c46278
JamesOdera <nyangoma.odera@gmail.com>
2022-06-07 02:11:20 -0700

    add file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+
+	return (0);
+}
