32d4c31069ff14bb48d0dc40cdc31534d62cdeab
JamesOdera <nyangoma.odera@gmail.com>
2022-06-07 09:06:40 -0700

    add file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - list natural nos below 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
