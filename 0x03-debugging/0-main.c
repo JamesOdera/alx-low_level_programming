d0c0d0ac7f015c38f916134f27d0dd5ec21ea7f0
JamesOdera <nyangoma.odera@gmail.com>
2022-06-08 02:29:52 -0700

    add file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * main - test for a positive_or_negative fxn
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
