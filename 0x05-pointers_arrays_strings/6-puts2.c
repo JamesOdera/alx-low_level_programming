e60e36b8b5b9aad06d4cf63790984bc407ebcf4a
JamesOdera <nyangoma.odera@gmail.com>
2022-06-13 01:13:13 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other xter of str to stdout
+ * @str: string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + 1))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+
+	_putchar('\n');
+}
