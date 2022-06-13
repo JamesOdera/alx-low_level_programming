65f820e1cb3388e13282ed7c1d0baf375cd0d4bb
JamesOdera <nyangoma.odera@gmail.com>
2022-06-13 02:05:19 -0700

    add file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * *_strcpy - copy str pointed to @src
+ * _strcpy - copy str pointed to @src
  * @dest: A buffer to copy the string to
  * @src: the source string to copy
  * Return: a pointer to the destination str @dest
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 
 	int i = 0;
