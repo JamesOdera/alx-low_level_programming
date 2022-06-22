d134168e278d97e6e8cceb107cc4339e7155f1a2
JamesOdera <nyangoma.odera@gmail.com>
2022-06-22 01:37:13 -0700

    add file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,7 @@
 
 int _strlen_recursion(char *s);
 {
-	int len = 0;
-
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
-	return (len);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
