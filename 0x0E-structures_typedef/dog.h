6c806a449f9e26aa4e1496a6232866b124e3ac02
JamesOdera <nyangoma.odera@gmail.com>
2022-07-04 02:35:10 -0700

    make changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - information about dogs
@@ -8,15 +8,16 @@
  * @owner: owner of dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
 
-typedef struct dog dog_t;
-
-int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
