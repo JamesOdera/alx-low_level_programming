d9c841027ae7a5982dd4ba1fbbaa4725e60b22d3
JamesOdera <nyangoma.odera@gmail.com>
2022-07-04 01:21:00 -0700

    initial commit

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * file: 0-object_like_macro
+ * desc: Heade file that defines a macro named SIZE
+ */
+
+#define SIZE 1024
+
+#endif
