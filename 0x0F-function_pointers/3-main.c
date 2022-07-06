aec62bcc7154f1c8b7d2d9b664ade0c5e7d9348c
JamesOdera <nyangoma.odera@gmail.com>
2022-07-06 10:33:07 -0700

    edit file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,11 +9,10 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,25 +20,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	/* added edge case if argv[2] was longer than 1 char*/
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-	printf("%d\n", ans);
 	return (0);
 }
