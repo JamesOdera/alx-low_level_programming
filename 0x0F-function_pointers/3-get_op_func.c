c054822751e82f0d53f0adae608afa754b255d99
JamesOdera <nyangoma.odera@gmail.com>
2022-07-06 10:06:13 -0700

    edit file

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,15 +19,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
-
-	i=0;
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			break;
+	int i = 0;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
 	return (ops[i].f);
 }
